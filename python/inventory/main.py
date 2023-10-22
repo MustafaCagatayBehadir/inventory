a1ddc1b94fea19b55620ab43f3bb24ed1bccb5b5
root <root@localhost.localdomain>
2023-10-22 23:48:24 +0300

    Started to develop huawei inventory.

--- python/inventory/main.py
+++ python/inventory/main.py
@@ -14,7 +14,30 @@ def get_kp_service_id(keypath: ncs.maagic.keypath._KeyPath) -> str:
     return service
 
 
-def get_device_live_status_inventory(root: ncs.maagic.Root, hostname: str, log: ncs.log.Log) -> ncs.maagic.List:
+def get_device_platform_name(root: ncs.maagic.Root, hostname: str, log: ncs.log.Log) -> str:
+    """Get device platform name."""
+    log.debug("Function ##" + INDENTATION * 2 + inspect.stack()[0][3])
+    platform = root.ncs__devices.device[hostname].platform.name
+    log.info("Device ##" + INDENTATION * 2 + hostname + " platform is " + platform)
+    return platform
+
+
+def populate_platform_grouping(inventory_name: str, device_hostname: str, log: ncs.log.Log) -> None:
+    """Populate device information under inventory device."""
+    log.debug("Function ##" + INDENTATION * 2 + inspect.stack()[0][3])
+    with ncs.maapi.single_write_trans(USER, 'system') as trans:
+        platform = ncs.maagic.get_node(trans, f"/ncs:devices/device{{{device_hostname}}}/platform")
+        inventory_manager = ncs.maagic.get_node(trans, f"/inv:inventory-manager{{{inventory_name}}}")
+        device_platform = inventory_manager.device[device_hostname].platform
+        device_platform.name = platform.name
+        device_platform.version = platform.version
+        device_platform.model = platform.model
+        device_platform.serial_number = platform.serial_number
+        log.info("Device ##" + INDENTATION * 2 + device_hostname + " platform details are setted.")
+        trans.apply()
+
+
+def iosxr_get_device_live_status_inventory(root: ncs.maagic.Root, hostname: str, log: ncs.log.Log) -> ncs.maagic.List:
     """Get device inventory data from ned live-status."""
     log.debug("Function ##" + INDENTATION * 2 + inspect.stack()[0][3])
     inventory_data = root.ncs__devices.device[hostname].live_status.cisco_ios_xr_stats__inventory
@@ -22,7 +45,7 @@ def get_device_live_status_inventory(root: ncs.maagic.Root, hostname: str, log: 
     return inventory_data
 
 
-def get_device_live_status_controllers(root: ncs.maagic.Root, hostname: str, log: ncs.log.Log) -> ncs.maagic.List:
+def iosxr_get_device_live_status_controllers(root: ncs.maagic.Root, hostname: str, log: ncs.log.Log) -> ncs.maagic.List:
     """Get device controllers data from ned live-status."""
     log.debug("Function ##" + INDENTATION * 2 + inspect.stack()[0][3])
     controllers_data = root.ncs__devices.device[hostname].live_status.cisco_ios_xr_stats__controllers
@@ -30,22 +53,7 @@ def get_device_live_status_controllers(root: ncs.maagic.Root, hostname: str, log
     return controllers_data
 
 
-def populate_device_grouping(inventory_name: str, device_hostname: str, log: ncs.log.Log) -> None:
-    """Populate device information under inventory device."""
-    log.debug("Function ##" + INDENTATION * 2 + inspect.stack()[0][3])
-    with ncs.maapi.single_write_trans(USER, 'system') as trans:
-        platform = ncs.maagic.get_node(trans, f"/ncs:devices/device{{{device_hostname}}}/platform")
-        inventory_manager = ncs.maagic.get_node(trans, f"/inv:inventory-manager{{{inventory_name}}}")
-        device = inventory_manager.device[device_hostname]
-        device.platform = platform.name
-        device.version = platform.version
-        device.model = platform.model
-        device.serial_number = platform.serial_number
-        log.info("Device ##" + INDENTATION * 2 + device_hostname + " platform details are setted.")
-        trans.apply()
-
-
-def populate_inventory_grouping(inventory_data: ncs.maagic.List, inventory_name: str, device_hostname: str,
+def iosxr_populate_inventory_grouping(inventory_data: ncs.maagic.List, inventory_name: str, device_hostname: str,
                                 log: ncs.log.Log) -> None:
     """Populate inventory list under inventory device."""
     log.debug("Function ##" + INDENTATION * 2 + inspect.stack()[0][3])
@@ -61,16 +69,16 @@ def populate_inventory_grouping(inventory_data: ncs.maagic.List, inventory_name:
         trans.apply()
 
 
-def populate_controllers_grouping(controllers_data: ncs.maagic.List, inventory_name: str, device_hostname: str,
-                                  log: ncs.log.Log) -> None:
+def iosxr_populate_controllers_grouping(controllers_data: ncs.maagic.List, inventory_name: str, device_hostname: str, 
+                                        log: ncs.log.Log) -> None:
     """Populate controllers list under inventory device."""
     log.debug("Function ##" + INDENTATION * 2 + inspect.stack()[0][3])
     with ncs.maapi.single_write_trans(USER, 'system') as trans:
+        inventory_manager = ncs.maagic.get_node(trans, f"/inv:inventory-manager{{{inventory_name}}}")
+        device = inventory_manager.device[device_hostname]
         for data in controllers_data.Optics:
             instance = data.instance
             transceiver = instance.transceiver_vendor_details
-            inventory_manager = ncs.maagic.get_node(trans, f"/inv:inventory-manager{{{inventory_name}}}")
-            device = inventory_manager.device[device_hostname]
             controller = device.controller.create(data.id)
             controller.controller_state = instance.controller_state
             controller.optics_type = transceiver.optics_type
@@ -81,6 +89,33 @@ def populate_controllers_grouping(controllers_data: ncs.maagic.List, inventory_n
             log.info("Controller ##" + INDENTATION * 4 + data.id + " is created.")
         trans.apply()
 
+
+def huawei_vrp_get_device_live_status_interface(root: ncs.maagic.Root, hostname: str, log: ncs.log.Log) -> ncs.maagic.List:
+    """Get device interface data from ned live-status."""
+    log.debug("Function ##" + INDENTATION * 2 + inspect.stack()[0][3])
+    interface_data = root.ncs__devices.device[hostname].live_status.vrp_stats__interface
+    log.info("Device ##" + INDENTATION * 2 + hostname + " interface data is gathered.")
+    return interface_data
+
+
+def huawei_vrp_get_device_live_status_transceiver(root: ncs.maagic.Root, hostname: str, log: ncs.log.Log) -> ncs.maagic.List:
+    """Get device transceiver data from ned live-status."""
+    log.debug("Function ##" + INDENTATION * 2 + inspect.stack()[0][3])
+    transceiver_data = root.ncs__devices.device[hostname].live_status.vrp_stats__transceiver
+    log.info("Device ##" + INDENTATION * 2 + hostname + " transceiver data is gathered.")
+    return transceiver_data
+
+
+def huawei_vrp_populate_controllers_grouping(interface_data: ncs.maagic.List, transceiver_data: ncs.maagic.List, 
+                                             inventory_name: str, device_hostname: str, log: ncs.log.Log) -> None:
+    """Populate controllers list under inventory device."""
+    log.debug("Function ##" + INDENTATION * 2 + inspect.stack()[0][3])
+    with ncs.maapi.single_write_trans(USER, 'system') as trans:
+        inventory_manager = ncs.maagic.get_node(trans, f"/inv:inventory-manager{{{inventory_name}}}")
+        device = inventory_manager.device[device_hostname]
+        for data in interface_data:
+            controller = device.controller.create
+            
 
 # ------------------------
 # Service CALLBACK
@@ -122,11 +157,21 @@ class InventoryUpdate(ncs.dp.Action):
         for device in devices:
             hostname = device.name if isinstance(device, ncs.maagic.ListElement) else device
             self.log.info("Sync ##" + INDENTATION * 2 + hostname)
-            inventory_data = get_device_live_status_inventory(root, hostname, self.log)
-            controllers_data = get_device_live_status_controllers(root, hostname, self.log)
-            populate_device_grouping(inventory_name, hostname, self.log)
-            populate_inventory_grouping(inventory_data, inventory_name, hostname, self.log)
-            populate_controllers_grouping(controllers_data, inventory_name, hostname, self.log)
+            platform = get_device_platform_name(root, hostname, self.log)
+            populate_platform_grouping(inventory_name, hostname, self.log)
+            
+            if platform == "ios-xr":
+                self.log.info("Device ##" + INDENTATION * 2 + hostname + " platform is ios-xr.")
+                inventory_data = iosxr_get_device_live_status_inventory(root, hostname, self.log)
+                controllers_data = iosxr_get_device_live_status_controllers(root, hostname, self.log)
+                iosxr_populate_inventory_grouping(inventory_data, inventory_name, hostname, self.log)
+                iosxr_populate_controllers_grouping(controllers_data, inventory_name, hostname, self.log)
+            
+            elif platform == "huawei-vrp":
+                self.log.info("Device ##" + INDENTATION * 2 + hostname + " platform is huawei-vrp.")
+                interface_data = huawei_vrp_get_device_live_status_interface(root, hostname, self.log)
+                transceiver_data = huawei_vrp_get_device_live_status_transceiver(root, hostname, self.log)
+
 
         output.result = f"Devices processed: {len(devices)}"
 
